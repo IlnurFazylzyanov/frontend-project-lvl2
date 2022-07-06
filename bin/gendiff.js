cfe0d0544bb84f05c5f613d9fdf17463aeebdcce
Ilnur_Fazylzyanov <ilnurka-1989.89@mail.ru>
2022-07-06 04:37:06 +0300

    performed output of arguments and options

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,7 +4,9 @@ import { Command } from 'commander';
 const program = new Command();
 
 program
-.description('Compares two configuration files and show a difference')
-.version('0.1.0');
+  .description('Compares two configuration files and show a difference')
+  .version('0.1.0')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>  output format')
 
 program.parse();
