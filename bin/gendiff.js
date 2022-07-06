f962dd05e530851123f6ea5643bbe214cc317510
Ilnur_Fazylzyanov <ilnurka-1989.89@mail.ru>
2022-07-06 07:37:19 +0300

    added two files for comparison and connected lodash

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,6 +7,6 @@ program
   .description('Compares two configuration files and show a difference')
   .version('0.1.0')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format <type>  output format')
+  .option('-f, --format <type>  output format');
 
 program.parse();
